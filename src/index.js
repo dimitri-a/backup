2b3e2074b808b4869743afbda48efeaf5d3e8438
Dimitri <couwenberg@gmail.com>
2018-06-20 12:31:09 +1000

    feat: app container redux connected and actions defined

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/ConfigureStore';
+import { Provider } from 'react-redux';
+
+const store = configureStore();
 
 ReactDOM.render(
-    <App />
-, document.getElementById('root'));
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
